a2d6eefd6da937c65f8afb605af70bc214a7b941
Pj Malva <pjmalva@gmail.com>
2020-04-18 12:36:16 -0300

    Sprint18

--- app/db/star.py
+++ app/db/star.py
@@ -12,9 +12,8 @@ class StarTwo:
 
     def makeSelect(self, sql):
         cursor = self.conn.cursor()
-        registers = cursor.execute(sql)
-        self.conn.close()
-        return registers
+        cursor.execute(sql)
+        return cursor
 
     def updateCategories(self):
         sql = """
@@ -26,7 +25,7 @@ class StarTwo:
         """
 
         self.registros = []
-        for registro in self.makeSelect(sql):
+        for registro in self.makeSelect(sql).fetchall():
             self.registros.append(
                 {
                     'code': registro[0],
@@ -36,7 +35,7 @@ class StarTwo:
         return self.registros
 
 
-    def updateProducts(self, minimunStock=1):
+    def updateProducts(self, minimunStock=0):
         sql = """
             SELECT CD.MERCADORIA, CD.NOME_M, CD.UR, CS.PRECO,
                 CS.PROMOCAO, CS.INI_PROM, CS.VAL_PROM,
@@ -60,20 +59,20 @@ class StarTwo:
         """
 
         self.registros = []
-        for registro in self.makeSelect(sql):
-            if float(registro[7]) >= minimunStock:
-                self.registros.append(
-                    {
-                        'reference': registro[0],
-                        'name': registro[1],
-                        'unity': registro[2],
-                        'price': registro[3],
-                        'price_sale': registro[4],
-                        'sale_start': registro[5],
-                        'sale_finish': registro[6],
-                        'stock': registro[7],
-                        'category_code': registro[8],
-                        'category_name': registro[9],
-                    }
-                )
+        for registro in self.makeSelect(sql).fetchall():
+            # if float(registro[7]) >= minimunStock:
+            self.registros.append(
+                {
+                    'reference': registro[0],
+                    'name': registro[1],
+                    'unity': registro[2],
+                    'price': registro[3],
+                    'price_sale': registro[4],
+                    'sale_start': registro[5],
+                    'sale_finish': registro[6],
+                    'stock': registro[7],
+                    'category_code': registro[8],
+                    'category_name': registro[9],
+                }
+            )
         return self.registros
